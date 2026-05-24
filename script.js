63d136cc27b4a55b84bec876ec503ed5a0eef631
inampudibhavyasri-sys <inampudibhavyasri@gmail.com>
2026-05-24 13:22:51 +0530

    Refactor scroll event listener for navbar

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-window.addEventListener("scroll", function () {
+
+window.addEventListener("scroll", function(){
 
     let navbar = document.getElementById("navbar");
 
-    if (window.scrollY > 50) {
+    if(window.scrollY > 50){
         navbar.classList.add("scrolled");
     }
 
-    else {
+    else{
         navbar.classList.remove("scrolled");
     }
 
